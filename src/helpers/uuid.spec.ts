04f975238899b0aeb0b35d21b6a5212eea174b78
Carl Winkler <carl@winkler.id.au>
2019-03-02 09:58:21 +0800

    Format codebase

--- src/helpers/uuid.spec.ts
+++ src/helpers/uuid.spec.ts
@@ -1,73 +1,73 @@
-import { isUuid, OPTIONAL } from "..";
-import { expect } from "chai";
-import { UuidOptions } from "./uuid";
+import { isUuid, OPTIONAL } from '..'
+import { expect } from 'chai'
+import { UuidOptions } from './uuid'
 
 interface Test {
-  it: string;
-  value: any;
-  expected: string | undefined | typeof OPTIONAL;
-  opts?: UuidOptions;
+  it: string
+  value: any
+  expected: string | undefined | typeof OPTIONAL
+  opts?: UuidOptions
 }
 const tests: Test[] = [
   {
-    it: "return undefined when given an arbitrary string",
-    value: "omg arbitrary",
+    it: 'return undefined when given an arbitrary string',
+    value: 'omg arbitrary',
     expected: undefined
   },
   {
-    it: "return undefined when given a missing string",
+    it: 'return undefined when given a missing string',
     value: undefined,
     expected: undefined
   },
   {
-    it: "return OPTIONAL when optional and given a missing string",
+    it: 'return OPTIONAL when optional and given a missing string',
     value: undefined,
     expected: OPTIONAL,
     opts: { optional: true }
   },
   {
-    it: "return undefined when optional and given an empty string",
-    value: "",
+    it: 'return undefined when optional and given an empty string',
+    value: '',
     expected: undefined,
     opts: { optional: true }
   },
   {
-    it: "return undefined when optional and given an arbitrary string",
-    value: "omg arbitrary again",
+    it: 'return undefined when optional and given an arbitrary string',
+    value: 'omg arbitrary again',
     expected: undefined,
     opts: { optional: true }
   },
   {
-    it: "return the string when given a valid UUID",
-    value: "5757c0e0-3b07-11e9-b210-d663bd873d93",
-    expected: "5757c0e0-3b07-11e9-b210-d663bd873d93",
+    it: 'return the string when given a valid UUID',
+    value: '5757c0e0-3b07-11e9-b210-d663bd873d93',
+    expected: '5757c0e0-3b07-11e9-b210-d663bd873d93',
     opts: { optional: true }
   },
   {
-    it: "return undefined when given a corrupt UUID (missing dash)",
-    value: "5757c0e0-3b07-11e9-b210d663bd873d93",
+    it: 'return undefined when given a corrupt UUID (missing dash)',
+    value: '5757c0e0-3b07-11e9-b210d663bd873d93',
     expected: undefined,
     opts: { optional: true }
   },
   {
-    it: "return undefined when given a corrupt UUID (short segment)",
-    value: "757c0e0-3b07-11e9-b210-d663bd873d93",
+    it: 'return undefined when given a corrupt UUID (short segment)',
+    value: '757c0e0-3b07-11e9-b210-d663bd873d93',
     expected: undefined,
     opts: { optional: true }
   },
   {
-    it: "return undefined when given a corrupt UUID (missing segment)",
-    value: "5757c0e0-3b07--b210-d663bd873d93",
+    it: 'return undefined when given a corrupt UUID (missing segment)',
+    value: '5757c0e0-3b07--b210-d663bd873d93',
     expected: undefined,
     opts: { optional: true }
   }
-];
+]
 
-describe("isUuid()", () => {
+describe('isUuid()', () => {
   for (const test of tests) {
     it(`will ${test.it}`, () => {
-      const actual = isUuid(test.value, test.opts);
-      expect(actual).to.equal(test.expected);
-    });
+      const actual = isUuid(test.value, test.opts)
+      expect(actual).to.equal(test.expected)
+    })
   }
-});
+})
--- src/helpers/uuid.ts
+++ src/helpers/uuid.ts
@@ -1,11 +1,11 @@
-import { OPTIONAL } from "./util";
+import { OPTIONAL } from './util'
 
 export interface UuidOptions {
-  optional?: boolean;
+  optional?: boolean
 }
 
 /** Valid uuid regex required by `isUuid()`. */
-const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
 
 /**
  * Validates a uuid as per RFC4122 using this code stolen from Stack Overflow:
@@ -14,17 +14,17 @@ const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[
  */
 export function isUuid(value: any, opts: UuidOptions = {}) {
   if (value === undefined) {
-    return opts.optional ? OPTIONAL : undefined;
+    return opts.optional ? OPTIONAL : undefined
   }
 
-  if (typeof value !== "string") {
-    return;
+  if (typeof value !== 'string') {
+    return
   }
 
-  const trimmed = value.trim();
+  const trimmed = value.trim()
   if (!trimmed) {
-    return undefined;
+    return undefined
   }
 
-  return uuidRegex.test(trimmed) ? trimmed : undefined;
+  return uuidRegex.test(trimmed) ? trimmed : undefined
 }
